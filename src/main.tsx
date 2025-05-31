509d947b124d5a86fb53d1d9cbd053e1f51f9fa8
PhamPhong137 <pp072003@gmail.com>
2025-05-31 18:49:31 +0700

    feat: sidebar + header + login + base dashboar

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router'
-import { router } from './router'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router';
+import { MantineProvider } from '@mantine/core';
+import { router } from './router';
 
-import '@/styles/index.css'
+import '@mantine/core/styles.css';
+import '@/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </StrictMode>
-)
+);
